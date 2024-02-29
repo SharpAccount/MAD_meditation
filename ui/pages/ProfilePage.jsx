5bc4fd29fe31759a0a6dc915206f9e556e433f9a
sharpaccount <vladikinastadiki@gmail.com>
2024-03-01 00:01:44 +0300

    fixed spme bugs and added react native splash screen

--- ui/pages/ProfilePage.jsx
+++ ui/pages/ProfilePage.jsx
@@ -9,10 +9,10 @@ export const ProfilePage = () => {
     return (
         <View style={{width:"100%", height:"100%", backgroundColor:"#253334", alignItems:"center"}}>
             <View style={{width: "90%"}}>
-                <View style={{display: "flex", justifyContent: "space-between", flexDirection: "row"}}>
-                    <Image source={require("../../assets/Logo (1).png")} style={{width: 10}}/>
+                <View style={{display: "flex", justifyContent: "space-between", flexDirection: "row", alignItems: "center"}}>    
+                    <Image source={require("../../assets/Logo (1).png")} style={{width: 43, height: 49}} resizeMode="contain"/>
                     <Pressable>
-                        <Text style={{fontFamily: "Alegreya Sans Medium"}}>exit</Text>
+                        <Text style={{fontFamily: "Alegreya Sans Medium", fontSize: 18, color: "#fff"}}>exit</Text>
                     </Pressable>
                 </View>
             </View>
